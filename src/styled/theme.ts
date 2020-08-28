fc80d4bbf328316df591f62217fbeecbc30d0b0d
Coyeah <Coyeah_chen@outlook.com>
2020-08-28 10:47:19 +0800

    style: 样式修改

--- src/styled/theme.ts
+++ src/styled/theme.ts
@@ -4,7 +4,7 @@ const theme: {
 } = {
   primary: '#00adb5',
   black: '#393e46',
-  white: '#eee',
+  white: '#e8e8e8',
   deepBlack: '#222831',
   gray: '#888',
   lightGray: '#ddd',
--- src/templates/bio.tsx
+++ src/templates/bio.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
-import { graphql, Link } from 'gatsby';
-import { TitleDiv, TimeDiv } from '../pages/index';
-import Basic from '../components/basic';
-import theme from '../styled/theme';
-import styles from './bio.module.less';
+import React from "react"
+import styled from "styled-components"
+import { graphql, Link } from "gatsby"
+import { TitleDiv, TimeDiv } from "../pages/index"
+import Basic from "../components/basic"
+import theme from "../styled/theme"
+import styles from "./bio.module.less"
 
 export const HrDiv = styled.div`
 width: 100%;
 border: 1px solid ${theme.lightGray};
 margin: 12px 0px;
 position: relative;
-`;
+`
 const PageFooterDiv = styled.div`
 display: inline-block;
 width: 50%;
 overflow:hidden; //超出的文本隐藏
 text-overflow:ellipsis; //溢出用省略号显示
 white-space:nowrap; //溢出不换行
-`;
+`
 const AvatarDiv = styled.div`
   position: absolute;
   height: 50px;
@@ -32,34 +32,34 @@ const AvatarDiv = styled.div`
   background-repeat: no-repeat;
   background-size: 100% 100%;
   background-clip: border-box;
-`;
+`
 
 const BioTemplate: React.FC<any> = ({ data, pageContext, location }) => {
-  const post = data.markdownRemark;
-  const { next, previous } = pageContext;
-  const { title, date, tag } = post.frontmatter;
+  const post = data.markdownRemark
+  const { next, previous } = pageContext
+  const { title, date, tag } = post.frontmatter
   const seo = {
     title,
     article: true,
-    pathname: location.pathname,
+    pathname: location.pathname
   }
   return (
     <Basic seo={seo} title={title}>
       <TitleDiv>{title}</TitleDiv>
       <TimeDiv>{date}</TimeDiv>
       {!!tag && <TimeDiv>{tag}</TimeDiv>}
-      <HrDiv />
-      <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.html }} />
-      <div style={{ marginTop: '4rem' }}>
-        <PageFooterDiv style={{ textAlign: 'left', paddingRight: '10%' }}>{next && (
-          <Link to={`/${next.frontmatter.path}`}>{'\< '}{next.frontmatter.title}</Link>
+      <HrDiv/>
+      <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.html }}/>
+      <div style={{ marginTop: "4rem" }}>
+        <PageFooterDiv style={{ textAlign: "left", paddingRight: "10%" }}>{next && (
+          <Link to={`/${next.frontmatter.path}`}>{"\< "}{next.frontmatter.title}</Link>
         )}</PageFooterDiv>
-        <PageFooterDiv style={{ textAlign: 'right', paddingLeft: '10%' }}>{previous && (
-          <Link to={`/${previous.frontmatter.path}`}>{previous.frontmatter.title}{' \>'}</Link>
+        <PageFooterDiv style={{ textAlign: "right", paddingLeft: "10%" }}>{previous && (
+          <Link to={`/${previous.frontmatter.path}`}>{previous.frontmatter.title}{" \>"}</Link>
         )}</PageFooterDiv>
       </div>
-      <HrDiv style={{marginTop: 4}}>
-        <AvatarDiv />
+      <HrDiv style={{ marginTop: 4 }}>
+        <AvatarDiv/>
       </HrDiv>
       <div className={styles.name}>coyeah</div>
       <div className={styles.desc}>前端学习路上的小记</div>
@@ -67,20 +67,20 @@ const BioTemplate: React.FC<any> = ({ data, pageContext, location }) => {
   )
 }
 
-export default BioTemplate;
+export default BioTemplate
 
 export const pageQuery = graphql`
-query PostBySlug($slug: String!) {
-  markdownRemark(fields: {slug: {eq: $slug}}, fileAbsolutePath: {regex: "/content\/blog/"}) {
-    frontmatter {
-      title
-      date(formatString: "MMMM DD, YYYY")
-      tag
+    query PostBySlug($slug: String!) {
+        markdownRemark(fields: {slug: {eq: $slug}}, fileAbsolutePath: {regex: "/content\/blog/"}) {
+            frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+                tag
+            }
+            html
+            fields {
+                slug
+            }
+        }
     }
-    html
-    fields {
-      slug
-    }
-  }
-}
-`;
+`
